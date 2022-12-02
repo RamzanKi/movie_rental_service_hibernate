175ba3924ce2dde435ce685f6f25e461d86192b6
ramzankikaev <sqrcloud@gmail.com>
2022-12-03 01:28:24 +0300

    fix all methods

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 import org.example.entities.Customer;
 import org.example.entities.Film;
 import org.example.entities.Rating;
+import org.example.entities.Store;
 import org.example.repository.MovieRentalServiceDB;
 
 import java.math.BigDecimal;
@@ -14,14 +15,14 @@ public class Main {
 
         Customer customer = db.findById((short) 130).orElse(null);
 //
-//        db.createNewCustomer("heeey", "dbasdw", "wqwe@@", db.getRandomStore(), "Test Street", "Nowhere", "Germany", "Berlin", "12345", "88005053535");
-//
-//        db.returnMovie((short)130, 11);
-//
+        db.createNewCustomer("heeey", "dbasdw", "wqwe@@", db.getRandomStore(), "Test Street", "Nowhere", "Germany", "Berlin", "12345", "88005053535");
+
+        db.returnMovie((short)130, 11);
+
         Film film = db.addNewFilm("my film", "good", 2022, (byte) 1, (byte) 7, new BigDecimal("5.99"), (short) 88, new BigDecimal("15.99"), Rating.getRatingByValue("PG-13"), "behind the scenes,trailers", db.getRandomCategorySet(), db.getRandomActorSet());
         System.out.println(film.toString());
-//
-//        Store randomStore = db.getRandomStore();
-//        db.rentFilmInStore(customer, randomStore, randomStore.getStaff(), "ACE GOLDFINGER");
+
+        Store randomStore = db.getRandomStore();
+        db.rentFilmInStore(customer, randomStore, randomStore.getStaff(), "ACE GOLDFINGER");
     }
 }
--- src/main/java/org/example/repository/MovieRentalServiceDB.java
+++ src/main/java/org/example/repository/MovieRentalServiceDB.java
@@ -40,7 +40,7 @@ public class MovieRentalServiceDB {
         properties.put(Environment.URL, "jdbc:p6spy:mysql://localhost:3306/movie");
         properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
         properties.put(Environment.USER, "root");
-        properties.put(Environment.PASS, "adminadmin");
+        properties.put(Environment.PASS, "root");
         properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
         properties.put(Environment.HBM2DDL_AUTO, "validate");
 
